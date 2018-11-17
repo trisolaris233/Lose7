c8a9bfa677c990900190bc8b50da77c93a5da94f
trisolaris233 <zhanshugegpu@163.com>
2018-11-17 14:13:52 +0800

    change the styles a bit, add toc powered by tocbot

--- functions.php
+++ functions.php
@@ -17,6 +17,38 @@ function themeConfig($form) {
 }
 
 
+function getRecentUpdate($num, $style = '<li><time style="margin-right: 10px; color: grey; font-size: 12px;">%date%</time><a href="%link%" title="%title%" target="_blank">%title%</a></li>') {
+    if($num) {
+        // 取出所有的文章
+        $db = Typecho_Db::get();
+        $select = $db->select()->from('contents')->where('type = ?', 'post')->order('modified', Typecho_Db::SORT_DESC);
+        $result = $db->fetchAll($select);
+        $i = 0;
+        $fields = array('%title%', '%date%', '%link%');
+
+        // 以降序输出文章标题
+        foreach ($result as $row) {
+            if($i >= $num && $num > 0)
+                break;
+            $row = Typecho_Widget::widget('Widget_Abstract_Contents')->push($row);
+            $post_title = htmlspecialchars($row['title']);
+            $permalink = $row['permalink'];
+            $modified_date = $row['modified'];
+            $values = array($post_title, date('Y-m-d', $modified_date), $permalink);
+            $tmp = $style;
+
+            for($i = 0; $i < count($fields); $i++) {
+                $tmp = str_replace($fields[$i], $values[$i], $tmp);
+            }
+            
+            echo $tmp;
+            
+            // echo '<li><time style="margin-right: 10px; color: grey; font-size: 12px;">'.date('Y-m-d', $modified_date).'</time><a href="'.$permalink.'" title="'.$post_title.'" target="_blank">'.$post_title.'</a></li>';
+        }
+    }
+}
+
+
 /*
 function themeFields($layout) {
     $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点LOGO地址'), _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO'));
--- post.php
+++ post.php
@@ -6,6 +6,69 @@
         height: 150px;
     }
 </style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.4.2/tocbot.min.js"></script>
+<script>
+    function appendHeadingId(selectors) {
+        $(selectors).each(function(){
+            $(this).attr("id", $(this).text());
+        });
+    }
+    $(document).ready(function () {
+        // 为h2, h3, h4添加id
+        appendHeadingId('h2,h3,h4');
+        
+        // 删除原先的sidebar， 重新添加sidebar与widget
+        $(".sidebar").remove();
+        $("#main").after('<div class="col-mb-12 col-4 kit-hidden-tb sidebar wow fadeInUp" id="secondary" role="complementary"></div>');
+        $(".sidebar").prepend('<section class="widget update-recent wow fadeInUp"><div class="title"><h3 class="widget-title"><?php _e('最近更新'); ?></h3></div>');
+        $(".update-recent").append('<ul class="widget-list"><?php getRecentUpdate(10, '<li class="wow fadeInUp"><time style="margin-right: 10px; color: grey; font-size: 12px;">%date%</time><a href="%link%" title="%title%" target="_blank">%title%</a></li>');?></ul>');
+        $(".sidebar").append('<section class="widget toc wow bounceIn"><div class="title"><h3 class="widget-title">文章目录</h3></div></section>');
+        $(".toc").append('<div class="toc-div"></div>')
+        
+        
+        // 实现sticky效果
+        var toc_top = $(".toc").offset().top;
+        var update_height = $(".update-recent").height();
+        var sidebar_width = $(".sidebar").width();
+        var rate_y = $(".sidebar").offset().left / $(document).width() * 100;
+        var insert_toc = false;
+        $(window).on('resize', function () { 
+            rate_y = $(".sidebar").offset().left / $(document).width() * 100;
+            var sidebar_width = $(".sidebar").width();
+        });
+        $(document).scroll(function () {
+            var st = document.body.scrollTop || document.documentElement.scrollTop;
+            if(st >= toc_top - 35) {
+                // 将sidebar位置固定
+                $(".sidebar").css({
+                    "position": "fixed",
+                    "top": (-(update_height + 50)).toString() + "px",
+                    "left": rate_y.toString() + "%",
+                    "max-width": sidebar_width,
+                });
+                // 如果还没有插入文章目录
+                if(!insert_toc) {
+                    // 插入文章目录
+                    tocbot.init({
+                        tocSelector: '.toc-div',
+                        contentSelector: '.post-single',
+                        headingSelector: 'h2, h3, h4',
+                        extraListClasses: 'wow fadeInUp animated'
+                    });
+                    insert_toc = true;
+                }
+            } else {
+                // 将sidebar位置还原
+                $(".sidebar").css({
+                    "position": "",
+                    "top": "",
+                    "left":"",
+                    "max-width":""
+                });
+            }
+        });
+    });
+</script>
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post-single" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h1>
